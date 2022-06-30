53a20912bb98fefce7ddb864039c0dff74624fbe
Carwoman <43983865+carmenpercar@users.noreply.github.com>
2022-06-30 23:34:56 +0200

    Hauter component

--- Assets/CarwVR/Resources/Scripts/Haunter/HaunterComponent.cs
+++ Assets/CarwVR/Resources/Scripts/Haunter/HaunterComponent.cs
@@ -7,14 +7,35 @@ public class HaunterComponent : MonoBehaviour
     [SerializeField] private LayerMask layerToCatch;
     [SerializeField] private Transform cameraTransform;
     [SerializeField] private float maxDistance;
-
-
+    [SerializeField] private float secondsToCatch;
+    private float elapsedSecondsLookingToCatchable;
+    private RaycastHit hitInfo; 
     private void Update() {
         
-        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, maxDistance, layerToCatch)) {
-            Debug.Log("Catchable detected");
+        if (DetectCollisionWithCatchableObject()) {
+            if (CheckIfCOuntDownOver()) {
+                Destroy(hitInfo.collider.gameObject);
+            }
+        }
+        else {
+            ResetCountDown();
         }
        
     }
+    private bool DetectCollisionWithCatchableObject() {
+        return Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hitInfo, maxDistance, layerToCatch);
+    }
+    private bool CheckIfCOuntDownOver() {
+        elapsedSecondsLookingToCatchable += Time.deltaTime;
+        if (elapsedSecondsLookingToCatchable >= secondsToCatch) {
+            ResetCountDown();
+            return true;
+        }
+        return false;
+    }
+    private void ResetCountDown() {
+        elapsedSecondsLookingToCatchable -= elapsedSecondsLookingToCatchable;
+    }
+    
 
 }
